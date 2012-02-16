46ed7b7fcbd4f4eaba97fe6cb22994cf68d246c6
James Brown <james@gulftobaytech.com>
2012-02-16 03:42:15 -0500

    Code cleanup before adding new code

--- samples/Node.js/app.js
+++ samples/Node.js/app.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var form = require('connect-form');
 var resumable = require('./resumable-node.js')('/tmp/resumable.js/');
-var app = express.createServer(form({keepExtensions:true}));
+var app = express.createServer(form({
+	keepExtensions: true
+}));
 
 // Host most stuff in the public folder
 app.use(express.static(__dirname + '/public'));
 
 // Handle uploads through Resumable.js
 app.post('/upload', function(req, res){
-           resumable.post(req, function(status, filename, original_filename, identifier){
-                            console.log('POST', status);
-                            res.send(status);
-                          });
-         });
+	resumable.post(req, function(status, filename, original_filename, identifier){
+		console.log('POST', status);
+		res.send(status);
+	});
+});
 // Handle status checks on chunks through Resumable.js
 app.get('/upload', function(req, res){
-          resumable.get(req, function(status, filename, original_filename, identifier){
-                          console.log('GET', status);
-                          res.send(status, (status=='found' ? 200 : 404));
-                        });
-        });
+	resumable.get(req, function(status, filename, original_filename, identifier){
+		console.log('GET', status);
+		res.send(status, (status == 'found' ? 200 : 404));
+	});
+});
 
 app.listen(3000);

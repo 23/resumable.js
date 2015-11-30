8a7dd2fd7b8ecb5611000c1df3fc285dc470f45b
Matthew de Nobrega <matt.denobrega@gmail.com>
2015-11-30 15:46:50 +0100

    Renamed type def file as per basarat's recommendation Added test file with example usage

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+declare class Resumable {
+    addFile(file: File, event: any): void;
+    assignBrowse(domNodes: any, isDirectory: boolean): void;
+    assignDrop(domNodes: any): void;
+    cancel(): void;
+    fire(): void;
+    getFromUniqueIdentifier(uniqueIdentifier: string): any;
+    getOpt(o: string): any;
+    getSize(): number;
+    handleChangeEvent(e: any): void;
+    handleDropEvent(e: any): void;
+    isUploading(): boolean;
+    on(event: string, callback: any): void;
+    pause(): void;
+    progress(): number;
+    removeFile(file: any): void;
+    unAssignDrop(domNodes: any): void;
+    upload(): void;
+    uploadNextChunk(): void;
+
+    defaults: Object;
+    events: Array<any>;
+    files: Array<File>;
+    opts: Object;
+    support: boolean;
+    version: number;
+
+    constructor(opts: Object);
+}
+
+export = Resumable
---
+++ resumable-tests.ts
@@ -0,0 +1,30 @@
+import Resumable = require('./index');
+
+let resumable: Resumable = new Resumable({chunkSize: 123});
+
+resumable.addFile(new File([], 'test.tmp'), {});
+resumable.assignBrowse(document, true);
+resumable.assignBrowse([document], true);
+resumable.assignDrop(document);
+resumable.assignDrop([document]);
+resumable.cancel();
+let defaults: Object = resumable.defaults;
+let events: any[] = resumable.events;
+let files: File[] = resumable.files;
+resumable.fire();
+let {} = resumable.getFromUniqueIdentifier('test');
+let {} = resumable.getOpt('test');
+let size:number = resumable.getSize();
+resumable.handleChangeEvent({});
+resumable.handleDropEvent({});
+let isUploading: boolean = resumable.isUploading();
+resumable.on('test', function() {});
+let opts: Object = resumable.opts;
+resumable.pause();
+let progress:number = resumable.progress();
+resumable.removeFile('TODO');
+let support: boolean = resumable.support;
+resumable.unAssignDrop({});
+resumable.upload();
+resumable.uploadNextChunk();
+let version:number = resumable.version;

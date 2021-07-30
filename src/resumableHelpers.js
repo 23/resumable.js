c4cf067837b8c659cbdbc9f8f9b7d7e8d2ad0101
pawelsoundso <pawel.gloeckner@student.hpi.uni-potsdam.de>
2021-07-30 16:29:16 +0200

    Remove unused code and fix the package in IE11

--- src/resumableHelpers.js
+++ src/resumableHelpers.js
@@ -19,10 +19,7 @@ export default class ResumableHelpers {
 		}
 	}
 
-	static generateUniqueIdentifier(file, event) {
-		if (typeof custom === 'function') {
-			return custom(file, event);
-		}
+	static generateUniqueIdentifier(file) {
 		var relativePath = file.webkitRelativePath || file.relativePath || file.fileName || file.name; // Some confusion in different versions of Firefox
 		var size = file.size;
 		return (size + '-' + relativePath.replace(/[^0-9a-zA-Z_-]/img, ''));

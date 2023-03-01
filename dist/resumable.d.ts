b7b61413604ffb991d3c26156eac227db172813f
Marcel Wendler <marcel.wendler@pointcloudtechnology.com>
2023-03-01 17:23:49 +0100

    Update build

--- dist/resumable.d.ts
+++ dist/resumable.d.ts
@@ -6,12 +6,24 @@ import { ResumableConfiguration } from './types/types';
  */
 export declare class Resumable extends ResumableEventHandler {
     private opts;
+    /**
+     * An object that contains one entry for every file category. The key is the category name, the value is an array of
+     * all ResumableFiles of that category that were added to this instance.
+     */
     private files;
+    /**
+     * Contains all file categories for which the upload was not yet completed.
+     */
+    private uncompletedFileCategories;
     private validators;
     private support;
     clearInput: boolean;
     dragOverClass: string;
-    fileTypes: string[];
+    fileCategories: string[];
+    defaultFileCategory: string | null;
+    fileTypes: string[] | {
+        [fileCategory: string]: string[];
+    };
     fileTypeErrorCallback: Function;
     _generateUniqueIdentifier: Function;
     maxFileSize?: number;
@@ -38,6 +50,7 @@ export declare class Resumable extends ResumableEventHandler {
      */
     protected setInstanceProperties(options: ResumableConfiguration): void;
     private sanitizeFileTypes;
+    private throwIfUnknownFileCategory;
     /**
      * Transforms a single fileEntry or directoryEntry item into a list of File objects this method is used to convert
      * entries found inside dragged-and-dropped directories.
@@ -61,7 +74,6 @@ export declare class Resumable extends ResumableEventHandler {
      * class to the element when a file is dropped onto it. In this case, we have to remove that class again before
      * calling "onDrop()".
      * If "onDrop()" is called from "handleDropEvent()" this is not needed.
-     *
      */
     private removeDragOverClassAndCallOnDrop;
     /**
@@ -80,13 +92,15 @@ export declare class Resumable extends ResumableEventHandler {
      * Validate and clean a list of files. This includes the removal of duplicates, a check whether the file type is
      * allowed and custom validation functions defined per file type.
      * @param {ExtendedFile[]} files A list of File instances that were previously extended with a uniqueIdentifier
+     * @param fileCategory The file category that has been provided for the files. Defaults to `defaultFileCategory`.
      */
     private validateFiles;
     /**
-     * Add an array of files to this instance's file list by creating new ResumableFiles. This includes a validation and
-     * deduplication of the provided array.
+     * Add an array of files to this instance's file list (of the file category, if given) by creating new ResumableFiles.
+     * This includes a validation and deduplication of the provided array.
      * @param fileList An array containing File objects
      * @param event The event with which the fileList was provided
+     * @param fileCategory The file category that has been provided for the file. Defaults to `defaultFileCategory`.
      */
     private appendFilesFromFileList;
     /**
@@ -94,6 +108,7 @@ export declare class Resumable extends ResumableEventHandler {
      * generator function.
      * @param file The file as an HTML 5 File object
      * @param event The event with which the file was provided originally
+     * @param fileCategory The file category that has been provided for the file. Defaults to `defaultFileCategory`.
      */
     private generateUniqueIdentifier;
     /**
@@ -101,26 +116,46 @@ export declare class Resumable extends ResumableEventHandler {
      */
     private uploadNextChunk;
     /**
+     * Returns all ResumableFiles of all file categories.
+     * The files are ordered by the order of the file categories in `this.fileCategories`. Files of the first category
+     * are added first, files of the second category are added second etc.
+     *
+     * @returns {ResumableFile[]} Array of all ResumableFiles that are stored for any category.
+     */
+    private getFilesOfAllCategories;
+    /**
      *  PUBLIC METHODS FOR RESUMABLE.JS
      *  This section only includes methods that should be callable from external packages.
      */
     /**
      * Assign a browse action to one or more DOM nodes. Pass in true to allow directories to be selected (Chrome only).
+     *
+     * @param domNodes The dom nodes to which the browse action should be assigned (can be an array or a single dom node).
+     * @param isDirectory If true, directories can be added via the file picker (Chrome only).
+     * @param fileCategory The file category that will be assigned to all added files. Defaults to `defaultFileCategory`.
      */
-    assignBrowse(domNodes: HTMLElement | HTMLElement[], isDirectory?: boolean): void;
+    assignBrowse(domNodes: HTMLElement | HTMLElement[], isDirectory?: boolean, fileCategory?: string): void;
     /**
      * Assign one or more DOM nodes as a drop target.
+     *
+     * @param domNodes The dom nodes to which the drop action should be assigned (can be an array or a single dom node).
+     * @param fileCategory The file category that will be assigned to all added files. Defaults to `defaultFileCategory`.
      */
-    assignDrop(domNodes: HTMLElement | HTMLElement[]): void;
+    assignDrop(domNodes: HTMLElement | HTMLElement[], fileCategory?: string): void;
     /**
      * Remove one or more DOM nodes as a drop target.
      */
     unAssignDrop(domNodes: HTMLElement | HTMLElement[]): void;
     /**
-     * Set the file types allowed to upload. Optionally pass a dom node on which the accepted file types should be
-     * updated as well.
+     * Set the file types allowed to upload.
+     * Per default the file types are updated for the default file category.
+     * Optionally pass a dom node on which the accepted file types should be updated as well.
+     *
+     * @param fileTypes String array of all allowed file types
+     * @param domNode An optional HTMLInputElement for which the "accepted" attribute should be updated accordingly.
+     * @param fileCategory The file category for which the file types should be updated. Defaults to `defaultFileCategory`.
      */
-    setFileTypes(fileTypes: string[], domNode?: HTMLInputElement): void;
+    setFileTypes(fileTypes: string[], domNode?: HTMLInputElement, fileCategory?: string): void;
     /**
      * Check whether any files are currently uploading
      */
@@ -144,11 +179,11 @@ export declare class Resumable extends ResumableEventHandler {
     /**
      * Add a HTML5 File object to the list of files.
      */
-    addFile(file: File, event: Event): void;
+    addFile(file: File, event: Event, fileCategory?: string): void;
     /**
      * Add a list of HTML5 File objects to the list of files.
      */
-    addFiles(files: File[], event: Event): void;
+    addFiles(files: File[], event: Event, fileCategory?: string): void;
     /**
      * Add a validator function for the given file type. This can e.g. be used to read the file and validate
      * checksums based on certain properties.
@@ -157,7 +192,7 @@ export declare class Resumable extends ResumableEventHandler {
      */
     addFileValidator(fileType: string, validator: Function): void;
     /**
-     * Cancel the upload of a specific ResumableFile object and remove it from the file list.
+     * Remove the given resumable file from the file list (of its corresponding file category).
      */
     removeFile(file: ResumableFile): void;
     /**
@@ -171,13 +206,14 @@ export declare class Resumable extends ResumableEventHandler {
     /**
      * Call the event handler for a DragEvent (when a file is dropped on a drop area).
      */
-    handleDropEvent(e: DragEvent): void;
+    handleDropEvent(e: DragEvent, fileCategory?: string): void;
     /**
      * Call the event handler for an InputEvent (i.e. received one or multiple files).
      */
-    handleChangeEvent(e: InputEvent): void;
+    handleChangeEvent(e: InputEvent, fileCategory?: string): void;
     /**
-     * Check whether the upload is completed, i.e. if all files were uploaded successfully.
+     * Check whether the upload is completed (if all files of a category are uploaded and if all files in general are
+     * uploaded).
      */
     checkUploadComplete(): void;
     /**
---
+++ dist/types/types.d.ts
@@ -0,0 +1,208 @@
+import ResumableFile from '../resumableFile';
+
+declare interface ExtendedFile extends File {
+  uniqueIdentifier?: string,
+  relativePath?: string,
+}
+
+declare const enum ResumableChunkStatus {
+  PENDING ='chunkPending',
+  UPLOADING = 'chunkUploading',
+  SUCCESS = 'chunkSuccess',
+  ERROR = 'chunkError',
+}
+
+declare interface ResumableConfiguration {
+  /**
+   * The target URL for the multipart POST request. This can be a string or a function that allows you to construct and return a value, based on supplied params. (Default: /)
+   **/
+  target?: string;
+  /**
+   * The size in bytes of each uploaded chunk of data. The last uploaded chunk will be at least this size and up to two the size, see Issue #51 for details and reasons. (Default: 1*1024*1024)
+   **/
+  chunkSize?: number;
+  /**
+   * Whether the value of the HTML element should be cleared after adding new files
+   */
+  clearInput ?: boolean;
+  /**
+   * The class name to add on drag over an assigned drop zone. (Default: `dragover`)
+   */
+  dragOverClass?: string;
+  /**
+   * Number of simultaneous uploads (Default: 3)
+   **/
+  simultaneousUploads?: number;
+  /**
+   * The name of the multipart POST parameter to use for the file chunk (Default: file)
+   **/
+  fileParameterName?: string;
+  /**
+   * The name of the chunk index (base-1) in the current upload POST parameter to use for the file chunk (Default: resumableChunkNumber)
+   */
+  chunkNumberParameterName?: string;
+  /**
+   * The name of the total number of chunks POST parameter to use for the file chunk (Default: resumableTotalChunks)
+   */
+  totalChunksParameterName?: string;
+  /**
+   * The time in milliseconds that defines the minimum time span between two progress callbacks
+   */
+  throttleProgressCallbacks?: number;
+  /**
+   * The name of the general chunk size POST parameter to use for the file chunk (Default: resumableChunkSize)
+   */
+  chunkSizeParameterName?: string;
+  /**
+   * The name of the total file size number POST parameter to use for the file chunk (Default: resumableTotalSize)
+   */
+  totalSizeParameterName?: string;
+  /**
+   * The name of the unique identifier POST parameter to use for the file chunk (Default: resumableIdentifier)
+   */
+  identifierParameterName?: string;
+  /**
+   * The name of the file category POST parameter to use for the file chunk (Default: resumableFileCategory)
+   */
+  fileCategoryParameterName?: string;
+  /**
+   * The name of the original file name POST parameter to use for the file chunk (Default: resumableFilename)
+   */
+  fileNameParameterName?: string;
+  /**
+   * The name of the file's relative path POST parameter to use for the file chunk (Default: resumableRelativePath)
+   */
+  relativePathParameterName?: string;
+  /**
+   * The name of the current chunk size POST parameter to use for the file chunk (Default: resumableCurrentChunkSize)
+   */
+  currentChunkSizeParameterName?: string;
+  /**
+   * The name of the file type POST parameter to use for the file chunk (Default: resumableType)
+   */
+  typeParameterName?: string;
+  /**
+   * Extra parameters to include in the multipart POST with data. This can be an object or a function. If a function, it will be passed a ResumableFile and a ResumableChunk object (Default: {})
+   **/
+  query?: Object;
+  /**
+   * Method for chunk test request. (Default: 'GET')
+   **/
+  testMethod?: string;
+  /**
+   * Method for chunk upload request. (Default: 'POST')
+   **/
+  uploadMethod?: string;
+  /**
+   * Extra prefix added before the name of each parameter included in the multipart POST or in the test GET. (Default: '')
+   **/
+  parameterNamespace?: string;
+  /**
+   * Extra headers to include in the multipart POST with data. This can be an object or a function that allows you to construct and return a value, based on supplied file (Default: {})
+   **/
+  headers?: Object | ((file: ResumableFile) => Object);
+  /**
+   * Method to use when POSTing chunks to the server (multipart or octet) (Default: multipart)
+   **/
+  method?: string;
+  /**
+   * Prioritize first and last chunks of all files. This can be handy if you can determine if a file is valid for your service from only the first or last chunk. For example, photo or video meta data is usually located in the first part of a file, making it easy to test support from only the first chunk. (Default: false)
+   **/
+  prioritizeFirstAndLastChunk?: boolean;
+  /**
+   * Make a GET request to the server for each chunks to see if it already exists. If implemented on the server-side, this will allow for upload resumes even after a browser crash or even a computer restart. (Default: true)
+   **/
+  testChunks?: boolean;
+  /**
+   * Override the function that generates unique identifiers for each file. (Default: null)
+   **/
+  generateUniqueIdentifier?: (file: ExtendedFile, event: Event, fileCategory: string) => string;
+  /**
+   * Indicates how many files can be uploaded in a single session. Valid values are any positive integer and undefined for no limit. (Default: undefined)
+   **/
+  maxFiles?: number;
+  /**
+   * A function which displays the please upload n file(s) at a time message. (Default: displays an alert box with the message Please n one file(s) at a time.)
+   **/
+  maxFilesErrorCallback?: (files: ResumableFile) => void;
+  /**
+   * The minimum allowed file size. (Default: undefined)
+   **/
+  minFileSize?: number;
+  /**
+   * A function which displays an error a selected file is smaller than allowed. (Default: displays an alert for every bad file.)
+   **/
+  minFileSizeErrorCallback?: (file: ResumableFile) => void;
+  /**
+   * The maximum allowed file size. (Default: undefined)
+   **/
+  maxFileSize?: number;
+  /**
+   * A function which displays an error a selected file is larger than allowed. (Default: displays an alert for every bad file.)
+   **/
+  maxFileSizeErrorCallback?: (file: ResumableFile) => void;
+  /**
+   * The file categories that will be used. Every file that is added to this resumable instance can be added to any of
+   * these categories.
+   * The order of the categories in this array also determines the order in which files are uploaded (files of first
+   * category are uploaded first). (Default: [], only the default category will be available.)
+   */
+  fileCategories?: string[];
+  /**
+   * The name of the default file category. This file category is always present, even when the fileCategories parameter
+   * is not set.
+   * If null is passed, the default category is not used. In this case, `fileCategories` must be set.
+   * (Default: 'default').
+   */
+  defaultFileCategory?: string | null;
+  /**
+   * The file types allowed to upload.
+   * If this is an array, the file types are used for all defined file catgories.
+   * Otherwise this needs to be an object, with an entry for every file category (category name as key, array of allowed
+   * file types as value).
+   * An empty array (either in the object of standalone) allows for any file type.
+   * Can also be changed later by calling setFileTypes(). (Default: [])
+   **/
+  fileTypes?: string[] | {[fileCategory: string]: string[]};
+  /**
+   * A function which displays an error a selected file has type not allowed. (Default: displays an alert for every bad file.)
+   **/
+  fileTypeErrorCallback?: (file: ResumableFile) => void;
+  /**
+   * A function which displays an error when the validator for a given file has failed
+   * @param file
+   */
+  fileValidationErrorCallback?: (file: ResumableFile) => void;
+  /**
+   * The maximum number of retries for a chunk before the upload is failed. Valid values are any positive integer and undefined for no limit. (Default: undefined)
+   **/
+  maxChunkRetries?: number;
+  /**
+   * The number of milliseconds to wait before retrying a chunk on a non-permanent error. Valid values are any positive integer and undefined for immediate retry. (Default: undefined)
+   **/
+  chunkRetryInterval?: number;
+  /**
+   * A list of HTTP errors that should be interpreted as failed requests
+   */
+  permanentErrors?: number[];
+  /**
+   * Standard CORS requests do not send or set any cookies by default. In order to include cookies as part of the request, you need to set the withCredentials property to true. (Default: false)
+   **/
+  withCredentials?: boolean;
+  /**
+   * The timeout in milliseconds for each request (Default: `0`)
+   */
+  xhrTimeout?: number;
+  /**
+   * The format of the chunk (Either Blob or base64)
+   */
+  chunkFormat?: string;
+  /**
+   * Whether or not the chunk content-type should be derived from original file.type.
+   */
+  setChunkTypeFromFile?: boolean;
+  /**
+   * The target URL for the GET request to the server for each chunk to see if it already exists.
+   */
+  testTarget?: string;
+}

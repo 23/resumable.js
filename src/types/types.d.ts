34398fc0bb480f61d3196e449d0655eba8a7b1a2
Marcel Wendler <marcel.wendler@pointcloudtechnology.com>
2023-03-01 09:49:00 +0100

    Fix type definition of generateUniqueIdentifier

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -116,7 +116,7 @@ declare interface ResumableConfiguration {
   /**
    * Override the function that generates unique identifiers for each file. (Default: null)
    **/
-  generateUniqueIdentifier?: () => string;
+  generateUniqueIdentifier?: (file: ExtendedFile, event: Event, fileCategory: string) => string;
   /**
    * Indicates how many files can be uploaded in a single session. Valid values are any positive integer and undefined for no limit. (Default: undefined)
    **/
